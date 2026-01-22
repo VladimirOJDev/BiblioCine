a8a504103e19f9d8a6f557653c339eef55655358
Vladimir Ortiz Dev <vladimir.ortiz.dev@gmail.com>
2026-01-22 11:55:22 -0600

    moviesSlideshow creado

--- lib/presentation/providers/movies/movies_slideshow_provider.dart
+++ lib/presentation/providers/movies/movies_slideshow_provider.dart
@@ -1,18 +1,9 @@
 import 'package:biblio_cine_app/domain/entities/movie.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'movies_providers.dart';
 
 part 'movies_slideshow_provider.g.dart';
 
-// final moviesSlideshowProvider = Provider<List<Movie>>((ref) {
-
-//   final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider); //se puede llamar un provider
-
-//   if(nowPlayingMovies.isEmpty)return[];
-
-//   return nowPlayingMovies.sublist(0,6);
-// }); 
 
 @riverpod 
 List<Movie> moviesSlideshow (Ref ref){
