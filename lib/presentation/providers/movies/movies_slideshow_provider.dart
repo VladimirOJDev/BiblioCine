bb62d4eb6dd2f94c5d0998dc36b019c9f0bfc274
Vladimir Ortiz Dev <vladimir.ortiz.dev@gmail.com>
2026-01-22 11:54:54 -0600

    moviesSlideshowProvider e initialLoadingProvider, han sido migrados

--- lib/presentation/providers/movies/movies_slideshow_provider.dart
+++ lib/presentation/providers/movies/movies_slideshow_provider.dart
@@ -1,12 +1,25 @@
 import 'package:biblio_cine_app/domain/entities/movie.dart';
-import 'package:biblio_cine_app/presentation/providers/movies/movies_providers.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'movies_providers.dart';
 
-final moviesSlideshowProvider = Provider<List<Movie>>((ref) {
+part 'movies_slideshow_provider.g.dart';
 
+// final moviesSlideshowProvider = Provider<List<Movie>>((ref) {
+
+//   final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider); //se puede llamar un provider
+
+//   if(nowPlayingMovies.isEmpty)return[];
+
+//   return nowPlayingMovies.sublist(0,6);
+// }); 
+
+@riverpod 
+List<Movie> moviesSlideshow (Ref ref){
   final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider); //se puede llamar un provider
 
   if(nowPlayingMovies.isEmpty)return[];
 
   return nowPlayingMovies.sublist(0,6);
-}); 
+}
+
