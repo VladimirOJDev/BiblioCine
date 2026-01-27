0c099772bb93fc970d1ec41d6ffd49e066cb901c
Vladimir Ortiz Dev <vladimir.ortiz.dev@gmail.com>
2026-01-27 12:11:31 -0600

    fix(actors-domain): correcion de nombre getNowPlaying -> getActorByMovie

--- lib/domain/repositories/actors_repository.dart
+++ lib/domain/repositories/actors_repository.dart
@@ -2,5 +2,5 @@
 import 'package:biblio_cine_app/domain/entities/actor.dart';
 
 abstract class ActorsRepository {
-  Future<List<Actor>> getNowPlaying({int movieId});
+  Future<List<Actor>> getActorByMovie(int movieId);
 }
