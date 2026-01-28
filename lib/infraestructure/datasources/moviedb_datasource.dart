4d125aa719d66b7d34f414033faf2858eced07c0
Vladimir Ortiz Dev <vladimir.ortiz.dev@gmail.com>
2026-01-28 11:22:29 -0600

    feat(search): contrato e eimplementación de la busqueda de peliculas

--- lib/infraestructure/datasources/moviedb_datasource.dart
+++ lib/infraestructure/datasources/moviedb_datasource.dart
@@ -99,5 +99,14 @@ class MoviedbDatasource extends MoviesDatasource{
     //Movie
     return movie;
   }
+  
+  @override
+  Future<List<Movie>> searchMovies(String query) async{
+
+    final response = await dio.get('/search/movie',
+    queryParameters: {'query':query}
+    );
+    return _jsonToMovies(response.data);
+  }
 
 }
